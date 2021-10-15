6a11cb943961141a0c81f9f5c6034262484cfb99
Ratinan Lee <lee.ratinan@gmail.com>
2021-10-15 14:12:10 +0800

    structure

---
+++ src/php/CurrencyFormatLocalizer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class CurrencyFormatLocalizer
+ */
+class CurrencyFormatLocalizer {
+
+    // https://www.thefinancials.com/Default.aspx?SubSectionID=curformat
+    // https://fastspring.com/blog/how-to-format-30-currencies-from-countries-all-over-the-world/
+    // https://www.dfa.cornell.edu/treasurer/cash-management/processinginternational/intl-currency
+
+}
