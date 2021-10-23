07eb09379bc50d2a5ea59ea7169ee8426e818972
Ratinan Lee <lee.ratinan@gmail.com>
2021-10-23 11:23:30 +0800

    add minified js

--- test/index.php
+++ test/index.php
@@ -25,7 +25,7 @@ if (empty($page))
           integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="../src/js/currencyFormatLocalizer.js"></script>
+    <script src="../src/js/currencyFormatLocalizer.min.js"></script>
   </head>
   <body>
     <header class="p-3 bg-dark text-white">
